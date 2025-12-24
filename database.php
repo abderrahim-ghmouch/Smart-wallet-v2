0999b179f985800e2610a5f64a0955a0d4389cf0
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2025-12-24 14:55:18 +0100

    connect the database

--- database.php
+++ database.php
@@ -1,12 +1,4 @@
-<?php
-$host="localhost";
-$dbname="smart-wallet";
-$username="abdo";
-$password="abdoabdo";
-try{
-
-    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-}catch(PDOException $e)
-{echo "your connection is failed".$e->getmessage();
-}
-
+<?php 
+    require_once __DIR__ ."/classes/Database.php";
+    $databse = new database("localhost","Smart-wallet","abdo","abdoabdo");
+    $pdo = $database->getConnection();
